c07d4e0c62076f6986490f9333171a5bc2f348f3
Namik Heydarov <heydarovnamiq@gmail.com>
2022-03-30 17:05:26 +0400

    changed

--- src/Components/Two.js
+++ src/Components/Two.js
@@ -8,7 +8,7 @@ function Two() {
            <h1 className='two-comp'>THIS IS 2ND COMPONENT</h1>
        </div>
         <div>
-            <img src='https://images.pexels.com/photos/9039662/pexels-photo-9039662.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' className='img-fluid'></img>
+            <img src='https://images.pexels.com/photos/2343170/pexels-photo-2343170.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' className='img-fluid'></img>
         </div>
        </>
     )
